5a2b0eab21f01ded0f994100b2c24d5b201824c9
Zeadrasil <thewitepheonix@gmail.com>
2025-05-20 19:21:30 -0600

    Implement Singleton

---
+++ Assets/Scripts/Singleton/GameManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindFirstObjectByType<GameManager>();
+                if(instance == null)
+                {
+                    instance = new GameObject().AddComponent<GameManager>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Print(string text)
+    {
+        Debug.Log(text);
+    }
+    public void ProcessUpButton()
+    {
+        Print("Pressed Up");
+    }
+    public void ProcessLeftButton()
+    {
+        Print("Pressed Left");
+    }
+    public void ProcessRightButton()
+    {
+        Print("Pressed Right");
+    }
+    public void ProcessDownButton()
+    {
+        Print("Pressed Down");
+    }
+
+    public void SayName()
+    {
+        Print(name);
+    }
+}
